96ea745e2c4c2ecc1bf467e5afd313abeab91b05
mohamed-aziz <me@aziz.tn>
2022-12-10 20:20:31 +0100

    Solve problem5

--- AoC22/Problem5.cs
+++ AoC22/Problem5.cs
@@ -6,37 +6,67 @@ namespace AoC22
     {
         public class Move
         {
-            
+            public int Number { get; init; }
+            public int From { get; init; }
+            public int To { get; init; }
+        }
+
+        public abstract class MovesFactory
+        {
+            public static List<Move> GetMoves(string input)
+            {
+                var moves = new List<Move>();
+                foreach (var line in input.Split(Environment.NewLine))
+                {
+                    var matches = new Regex(@"^move (\d+) from (\d+) to (\d+)$").Matches(line);
+                    matches.Cast<Match>().ToList().ForEach(m => moves.Add(new Move
+                    {
+                        Number = int.Parse(m.Groups[1].Value),
+                        From = int.Parse(m.Groups[2].Value),
+                        To = int.Parse(m.Groups[3].Value)
+                    }));
+                }
+                
+                return moves;
+            }
         }
         
-        public class Problem5: IProblem<string>
+        public class Problem5
         {
-            private List<Stack<char>> GetStacks(string input)
+
+            public string Solve(List<Stack<Char>> stacks, List<Move> moves)
             {
-                var transposedString = "";
-
-                var stacks = new List<Stack<char>>();
-                var matches = new Regex(@"\[([A-Z])\]", RegexOptions.Singleline).Matches(input);
-
-                foreach (Match match in matches)
+                foreach (var move in moves)
                 {
-                    Console.WriteLine(match.Value);
+                    for (var i = 0; i < move.Number; i++)
+                    {
+                        var item = stacks[move.From - 1].Pop();
+                        stacks[move.To - 1].Push(item);
+                    }
                 }
-                return stacks;
-            } 
-            
-            
-            public string Solve(string input)
-            {
-                var crates = new List<Stack<char>>();
-                GetStacks(input);
-
-                return "";
+                
+                return string.Join("", stacks.Select(s => s.Count != 0 ? s.Peek().ToString() : ""));
             }
 
-            public string Solve2(string input)
+            public string Solve2(List<Stack<Char>> stacks, List<Move> moves)
             {
-                throw new NotImplementedException();
+                foreach (var move in moves)
+                {
+                    var intermediateStack = new Stack<char>();
+                    for (var i = 0; i < move.Number; i++)
+                    {
+                        var item = stacks[move.From - 1].Pop();
+                        intermediateStack.Push(item);
+                    }
+
+                    while (intermediateStack.Count != 0)
+                    {
+                        var item = intermediateStack.Pop();
+                        stacks[move.To - 1].Push(item);    
+                    }
+                }
+                
+                return string.Join("", stacks.Select(s => s.Count != 0 ? s.Peek().ToString() : ""));
             }
         }         
     }
