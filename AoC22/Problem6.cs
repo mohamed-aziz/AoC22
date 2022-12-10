8c8ca2cc3751d7dc044bcb2af83fff8302f0f2fa
mohamed-aziz <me@aziz.tn>
2022-12-10 20:46:09 +0100

    Solve problem 6

---
+++ AoC22/Problem6.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace AoC22
+{
+    namespace Problem6
+    {
+        public class Problem6: IProblem<long>
+        {
+            public long Helper(string input, int packetLength)
+            {
+                var i = 0;
+                var heap = new List<char>();
+                while (i < input.Length)
+                {
+                    var item = input[i];
+
+                    if (heap.Contains(item))
+                    {
+                        var idx = heap.IndexOf(item);
+                        for (var j = 0; j <= idx; j++)
+                        {
+                            heap.RemoveAt(0);
+                        }
+                    }
+                    heap.Add(item);
+                    ++i;
+                    
+                    if (heap.Count == packetLength)
+                    {
+                        break;
+                    }
+                }
+
+                return i;
+            } 
+            public long Solve(string input)
+            {
+                return Helper(input, 4);
+            }
+
+            public long Solve2(string input)
+            {
+                return Helper(input, 14);
+            }
+        }        
+    }
+}
+
+
---
+++ Tests/TestProblem6.cs
@@ -0,0 +1,35 @@
+﻿using AoC22.Problem6;
+
+namespace Tests;
+
+[TestFixture]
+public class TestProblem6
+{
+    private Problem6 _problem;
+    [SetUp]
+    public void SetUp()
+    {
+        _problem = new Problem6();
+    }
+
+    [Test]
+    public void TestPart1()
+    {
+        Assert.That(_problem.Solve("bvwbjplbgvbhsrlpgdmjqwftvncz"), Is.EqualTo(5));
+        Assert.That(_problem.Solve("nppdvjthqldpwncqszvftbrmjlhg"), Is.EqualTo(6));
+        Assert.That(_problem.Solve("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), Is.EqualTo(10));
+        Assert.That(_problem.Solve("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), Is.EqualTo(11));
+    }
+
+    [Test]
+    public void SolvePart1()
+    {
+        Assert.That(_problem.Solve(File.ReadAllText(@"Inputs\\day6.txt")), Is.EqualTo(1093));
+    }
+    
+    [Test]
+    public void SolvePart2()
+    {
+        Assert.That(_problem.Solve2(File.ReadAllText(@"Inputs\\day6.txt")), Is.EqualTo(3534));
+    }
+}
