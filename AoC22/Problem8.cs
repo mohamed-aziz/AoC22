88714e722c176c3f68e83132038a3a65e4789075
mohamed-aziz <me@aziz.tn>
2022-12-10 23:14:31 +0100

    Finish Day8 Part1

---
+++ AoC22/Problem8.cs
@@ -0,0 +1,143 @@
+﻿namespace AoC22
+{
+    namespace Problem8
+    {
+        
+        public class Grid
+        {
+            private List<List<int>> _grid = new(); 
+            public Grid(string input)
+            {
+                foreach (var line in input.Split(Environment.NewLine))
+                {
+                    _grid.Add(line.Select(x => int.Parse(x.ToString())).ToList());
+                }
+            }
+
+            public int GetGridAt(int x, int y)
+            {
+                if (x < 0 || y < 0 || x >= GetGridWidth() || y >= GetGridHeight())
+                {
+                    return -1;
+                }
+                
+                return _grid[y][x];
+            }
+            
+            public int GetGridHeight()
+            {
+                return _grid.Count;
+            }
+            
+            public int GetGridWidth()
+            {
+                return _grid[0].Count;
+            }
+
+            public int GetBiggestLeft(int x, int y)
+            {
+                int biggest = -1;
+                for (int i = 0; i < x; i++)
+                {
+                    if (GetGridAt(i, y) > biggest)
+                    {
+                        biggest = GetGridAt(i, y);
+                    }
+                }
+
+                return biggest;
+            }
+            
+            public int GetBiggestRight(int x, int y)
+            {
+                int biggest = -1;
+                for (int i = x + 1; i < GetGridWidth(); i++)
+                {
+                    if (GetGridAt(i, y) > biggest)
+                    {
+                        biggest = GetGridAt(i, y);
+                    }
+                }
+
+                return biggest;
+            }
+            
+            public int GetBiggestDown(int x, int y)
+            {
+                int biggest = -1;
+                for (int i = y + 1; i < GetGridHeight(); i++)
+                {
+                    if (_grid[i][x] > biggest)
+                    {
+                        biggest = _grid[i][x];
+                    }
+                }
+
+                return biggest;
+            }
+
+            public int GetBiggestUp(int x, int y)
+            {
+                int biggest = -1;
+                for (int i = 0; i < y; i++)
+                {
+                    if (_grid[i][x] > biggest)
+                    {
+                        biggest = _grid[i][x];
+                    }
+                }
+
+                return biggest;
+            }
+
+            public bool IsVisibleFromUp(int x, int y)
+            {
+                return GetGridAt(x, y) > GetBiggestUp(x, y);
+            }
+
+            public bool IsVisibleFromDown(int x, int y)
+            {
+                return GetGridAt(x, y) > GetBiggestDown(x, y);
+            }
+
+            public bool IsVisibleFromLeft(int x, int y)
+            {
+                return GetGridAt(x, y) > GetBiggestLeft(x, y);
+            }
+
+            public bool IsVisibleFromRight(int x, int y)
+            {
+                return GetGridAt(x, y) > GetBiggestRight(x, y);
+            }
+
+        }
+        
+        public class Problem8: IProblem<long>
+        {
+            public long Solve(string input)
+            {
+                var grid = new Grid(input);
+                var count = 0;
+                for (int i = 0; i < grid.GetGridWidth(); i++)
+                {
+                    for (int j = 0; j < grid.GetGridHeight(); j++)
+                    {
+                        if (grid.IsVisibleFromDown(i, j) || grid.IsVisibleFromUp(i, j) || grid.IsVisibleFromRight(i, j)
+                            || grid.IsVisibleFromLeft(i, j))
+                        {
+                            ++count;
+                        }
+                    }
+                }
+
+                return count;
+            }
+
+            public long Solve2(string input)
+            {
+                throw new NotImplementedException();
+            }
+        }        
+    }
+}
+
---
+++ Tests/TestProblem8.cs
@@ -0,0 +1,34 @@
+﻿using AoC22.Problem8;
+
+namespace Tests;
+
+[TestFixture]
+public class TestProblem8
+{
+    private Problem8 _problem;
+    [SetUp]
+    public void SetUp()
+    {
+        _problem = new Problem8();
+    }
+
+    [Test]
+    public void TestPart1()
+    {
+        var input = @"30373
+25512
+65332
+33549
+35390";
+        var result = _problem.Solve(input);
+        Assert.That(result, Is.EqualTo(21));
+    }
+
+    [Test]
+    public void SolvePart1()
+    {
+        var input = File.ReadAllText(@"Inputs\\day8.txt");
+        var result = _problem.Solve(input);
+        Assert.That(result, Is.EqualTo(1698));
+    }
+}
